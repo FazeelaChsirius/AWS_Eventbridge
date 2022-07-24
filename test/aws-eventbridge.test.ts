1daa03db9c72ab52dcc12b9decf50052ddde3991
fazeli-ch <fazeelamushtaq076@gmail.com>
2022-07-24 07:46:19 -0400

    Initial commit

---
+++ test/aws-eventbridge.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEventbridge from '../lib/aws-eventbridge-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-eventbridge-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEventbridge.AwsEventbridgeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
